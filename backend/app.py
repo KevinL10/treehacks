96515c911621f104a3245df9f0fc2e1da0b5ec31
Kevin Liu <kevinsliu0@gmail.com>
2024-02-17 19:22:50 -0800

    change backend to fastapi

--- backend/app.py
+++ backend/app.py
@@ -1,53 +1,96 @@
-import socketio
-from typing import Dict
+from fastapi import FastAPI, WebSocket, Request
+from fastapi.responses import HTMLResponse, JSONResponse, PlainTextResponse
 from collections import defaultdict
+from uuid import uuid4
+from typing import List, Dict   
+import asyncio
 import time
-import logging
+
+app = FastAPI()
+
+# room_id: list of names in the room
+# note: names server as unique identifiers; two players must not have the same name
+room_users = defaultdict(list)
+
+# room_id: event
+room_started_event: Dict[int, asyncio.Event] = {}
+
+
+
+# maps from room_id: {name: [(health, step, time)]}
+user_health_data = defaultdict(lambda: defaultdict(list))
+
+
+@app.get("/")
+async def index():
+    return JSONResponse({"status": "ok"})
+
+
+@app.post("/start")
+async def start_room(request: Request):
+    data = await request.json()
+    room_id = data["room_id"]
+    room_started_event[room_id].set()
+    return JSONResponse({"status": "ok"})
+
+
+@app.get("/data")
+async def get_room_data(room_id: int = -1):
+    print(user_health_data[room_id])
+    return JSONResponse(user_health_data[room_id])
+
+
+@app.websocket("/ws")
+async def websocket_endpoint(websocket: WebSocket):
+    await websocket.accept()
+
+    # join room with room_id and name
+    data = await websocket.receive_json()
+    assert data["method"] == "join_room"
+    
+    room_id = data["params"]["room_id"]
+    name = data["params"]["name"]
+    room_users[room_id].append(name)
+
+    await websocket.send_json({"method": "update_status", "params": {"status": "waiting"}})
+
+    # wait for game to be started
+    game_started = asyncio.Event()
+    room_started_event[room_id] = game_started 
+    await game_started.wait()
+    
+    await websocket.send_json({"method": "update_status", "params": {"status": "starting"}})
+    
+    # receive data from watch repeatedly
+    # TODO: stop when the song finishes
+    while True:
+        data = await websocket.receive_json()
+        assert data["method"] == "submit_data"
+
+        heartrate = data["params"]["heartrate"]
+        step_count = data["params"]["step_count"]
+        timestamp = int(time.time())
+
+        print(data)
+        user_health_data[room_id][name].append((heartrate, step_count, timestamp))
+
+
 
 '''
 
-join_room (sid, room_id): joins a given room   
+accept() name
 
-data (sid, data): sends an update on data 
+send() ok
+
+
+send() start
+
+while ...
+accept() data
+
+send() done
+
+
+
+
 '''
-
-sio = socketio.Server()
-app = socketio.WSGIApp(sio)
-
-user_data = defaultdict(list) 
-
-
-logging.basicConfig(level=logging.INFO)
-logger = logging.getLogger("app")
-
-@sio.event
-def connect(sid, environ):
-    logger.info("logger: Client connected:" + sid)
-    print("Client connected:", sid)
-    sio.emit('response', 'Connected to server', room=sid)
-
-
-@sio.event 
-def submit_data(sid, data: Dict):
-    logger.info(f"logger: User {sid} uploaded data: {data}")
-    print(f"User {sid} uploaded data: {data}")
-    user_data[sid].append(data)
-
-@sio.event
-def join_room(sid, room_id):
-    logger.info(f"logger: User {sid} has joined room {room_id}")
-    print(f"User {sid} has joined room {room_id}")
-    sio.enter_room(sid, room_id)
-
-    # simulate time until room starts
-    # time.sleep(10)
-    # sio.emit('status', "STARTED")
-
-@sio.event
-def disconnect(sid):
-    print("User disconnected", sid)
-
-if __name__ == '__main__':
-    import eventlet
-    eventlet.wsgi.server(eventlet.listen(('', 8000)), app)
-
--- backend/client.py
+++ backend/client.py
@@ -1,22 +1,41 @@
-import socketio
+import websockets
+import asyncio
+import json
 import time
 
-sio = socketio.Client()
+async def main():
+    uri = "ws://localhost:8000/ws"
+    async with websockets.connect(uri) as websocket:
+        await websocket.send(json.dumps({"method": "join_room", "params": {"room_id": 123456, "name": "Kevin"}}))
+        
+        # check waiting response
+        data = await websocket.recv()
+        data = json.loads(data)
+        assert data["method"] == "update_status"
+        assert data["params"]["status"] == "waiting"
+        print("Joined room")
 
-@sio.event
-def connect():
-    print('Connected to server')
+        # wait until room starts 
+        data = await websocket.recv()
+        print(data)
+        data = json.loads(data)
+        assert data["method"] == "update_status"
+        assert data["params"]["status"] == "starting"
 
-@sio.event
-def response(message):
-    print('Message from server:', message)
 
-@sio.event
-def disconnect():
-    print('Disconnected from server')
+        mock_data = {"heartrate": 120, "step_count": 50}
+        await websocket.send(json.dumps({"method": "submit_data", "params": mock_data}))
 
-sio.connect('http://localhost:8000')
-sio.emit('join_room', 1234)
-sio.emit('submit_data', {"heart_rate": 60})
+        time.sleep(10)
 
-time.sleep(5)
+'''
+start room: `curl -X POST -H "Content-Type: application/json" -d '{"room_id": 123456}' localhost:8000/start`
+
+get data: `curl http://localhost:8000/data?room_id=123456`
+
+'''
+
+
+if __name__ == "__main__":
+    asyncio.get_event_loop().run_until_complete(main())
+    # asyncio.run(send_message)
